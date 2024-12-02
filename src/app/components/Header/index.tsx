000c5f6bd81004f3a8ed88dee8a54f324034c1c0
Madhur Goyal <madhur.goyal2@gmail.com>
2024-12-03 02:03:19 +0530

    ADD: Home Button in Header

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,4 +1,8 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
 import HeaderIconButton from './icon-button';
+import { appRoutes } from '@/app/common/constants';
 
 export default function Header({
   onToggleSidebar,
@@ -11,12 +15,17 @@ export default function Header({
   isDarkMode: boolean;
   onToggleDarkMode: () => void;
 }) {
+  const router = useRouter();
   return (
     <div className='bg-white flex h-14 border-b px-4 py-2 items-center justify-between dark:bg-backgroundDark dark:border-zinc-800'>
       <div className='flex items-center gap-2'>
         <HeaderIconButton
           icon={isSidebarOpen ? 'fa-outdent' : 'fa-indent'}
           onClick={onToggleSidebar}
+        />
+        <HeaderIconButton
+          icon='fa-home'
+          onClick={() => router.push(appRoutes.home)}
         />
 
         <div className='flex items-center gap-2 bg-slate-100 dark:bg-zinc-700 h-full p-2 rounded-md'>
